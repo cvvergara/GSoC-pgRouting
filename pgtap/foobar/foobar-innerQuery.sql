e6e4844b3cf84f9ea72946547fc7aa67d6101738
cvvergara <vicky@georepublic.de>
2019-05-27 09:01:47 -0500

    [foobar] double check design

--- pgtap/foobar/foobar-innerQuery.sql
+++ pgtap/foobar/foobar-innerQuery.sql
@@ -6,22 +6,22 @@ SELECT plan(50);
 SELECT has_function('pgr_foobar');
 SELECT todo_start('design needs to work');
 SELECT has_function('pgr_foobar',
-    ARRAY['text', 'bigint', 'bigint', 'integer', 'boolean', 'bigint']);
+    ARRAY['text', 'bigint', 'bigint', 'boolean', 'bigint']);
 
 SELECT function_returns('pgr_foobar',
-    ARRAY['text', 'bigint', 'bigint', 'integer', 'boolean', 'bigint'],
+    ARRAY['text', 'bigint', 'bigint', 'boolean', 'bigint'],
     'setof record');
 
 -- parameter names
 SELECT set_eq(
     $$SELECT  proargnames FROM pg_proc WHERE proname = 'pgr_foobar'$$,
-    $$SELECT  '{"","","","","directed","number","seq","path_id","path_seq","node","edge","cost"}'::TEXT[] $$
+    $$SELECT  '{"","","","directed","number","seq","path_id","path_seq","node","edge","cost"}'::TEXT[] $$
 );
 
 -- parameter types
 SELECT set_eq(
     $$SELECT  proallargtypes FROM pg_proc WHERE proname = 'pgr_foobar'$$,
-    $$SELECT  '{25,20,20,23,16,20,23,23,23,20,20,701}'::OID[] $$
+    $$SELECT  '{25,20,20,16,20,23,23,23,20,20,701}'::OID[] $$
 );
 
 
